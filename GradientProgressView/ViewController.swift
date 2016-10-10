5283abe957c97892c3344e85ca2f63cdf1e8d992
Josh Sklar <jrmsklar@gmail.com>
2016-10-10 15:48:20 -0400

    enhance demo screen with color and value options

--- GradientProgressView/ViewController.swift
+++ GradientProgressView/ViewController.swift
@@ -10,30 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var pv: GradientProgressView!
+    @IBOutlet weak var progressView: GradientProgressView!
+    @IBOutlet weak var progressSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        pv = GradientProgressView(frame: CGRectZero)
-        pv.state = GradientProgressView.State(progress: 0.4, progressColor: UIColor.blueColor(), backgroundColor: UIColor.greenColor(), borderColor: UIColor.grayColor())
-        pv.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(pv)
-        
-        let views = ["pv": pv]
-        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[pv]-|", options: [], metrics: nil, views: views))
-        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-100-[pv(50)]", options: [], metrics: nil, views: views))
-        
+        var state = progressView.state
+        state.progress = CGFloat(progressSlider.value)
+        progressView.state = state
     }
     
-    
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func didTapColorButton(sender: UIButton) {
+        var state = progressView.state
+        state.progressColor = sender.backgroundColor!
+        progressView.state = state
     }
-
-
+    
+    @IBAction func progressSliderChanged(sender: UISlider) {
+        var state = progressView.state
+        state.progress = CGFloat(sender.value)
+        progressView.state = state
+    }
 }
 
